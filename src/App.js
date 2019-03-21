64da3f873f702c9c8439147bedb4ff9e7a3cd52a
Steven Thomson <darccide@protonmail.com>
2019-03-22 00:14:26 +0900

    clean up and ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Library from './components/Library';
+import Landing from './components/Landing';
+import Album from './components/Album';
+import Logo from './data/images/bloc_jams_logo.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-container">
+        <div className="App-header">
+          <div className="navbar fixed-top">
+            <a href="/" className="navbar-brand">
+              <img
+                src={Logo}
+                alt="Bloc Jams Logo"
+                style={{ height: '2.2em' }}
+              />
+            </a>
+            <a href="/Library" className="library-link">
+              Library
+            </a>
+          </div>
+        </div>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
+        </main>
+        <footer className="App-footer" />
       </div>
     );
   }
